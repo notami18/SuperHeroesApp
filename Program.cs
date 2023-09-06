4b03eca0019e72207e46b4e2923173f51e22b6a3
Andres Minota <minocamc-18@hotmail.com>
2023-09-06 17:00:40 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var superMan = new SuperHeroe();
+superMan.Id = 1;
+superMan.Name = "Sperma";
+superMan.SecretIdentity = "Clark Kent";
+superMan.city = "Metropolis";
+superMan.Flying = true;
+superMan.SuperPowers = new[] { "Vision lazer", "Super fuerza", "Volar", "Vision de rayos X" };
+
+
+class SuperHeroe
+{
+    public int Id;
+    public string? Name;
+    public string? SecretIdentity;
+    public string? city;
+    public string[]? SuperPowers;
+    public bool Flying;
+
+}
