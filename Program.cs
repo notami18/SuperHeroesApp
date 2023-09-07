e1255854ade04ad141781d617e480d2d4ba01427
Andres Minota <andresnotami@gmail.com>
2023-09-06 20:31:14 -0500

    🚧 creating more complex classes

--- Program.cs
+++ Program.cs
@@ -1,22 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var superMan = new SuperHeroe();
-superMan.Id = 1;
-superMan.Name = "Sperma";
-superMan.SecretIdentity = "Clark Kent";
-superMan.city = "Metropolis";
-superMan.Flying = true;
-superMan.SuperPowers = new[] { "Vision lazer", "Super fuerza", "Volar", "Vision de rayos X" };
+var canFly = new SuperPower
+{
+    Name = "Volar",
+    Description = "Capacidad de poder volar y planear en el aire",
+    Level = LevelPower.LevelTwo
+};
 
+var superPower = new SuperPower
+{
+    Name = "Super Fuerza",
+    Level = LevelPower.LevelFive
+};
+
+var superMan = new SuperHeroe
+{
+    Id = 1,
+    Name = "Sperma",
+    SecretIdentity = "Clark Kent",
+    City = "Metropolis",
+    Flying = true
+};
+
+List<SuperPower> supermanPower = new()
+{
+    canFly,
+    superPower
+};
+
+superMan.SuperPowers = supermanPower;
 
 class SuperHeroe
 {
     public int Id;
     public string? Name;
     public string? SecretIdentity;
-    public string? city;
-    public string[]? SuperPowers;
+    public string? City;
+    public List<SuperPower>? SuperPowers;
     public bool Flying;
+}
 
+class SuperPower
+{
+    public string? Name;
+    public string? Description;
+    public LevelPower Level;
+}
+
+enum LevelPower
+{
+    LevelOne,
+    LevelTwo,
+    LevelThree,
+    LevelFour,
+    LevelFive,
 }
